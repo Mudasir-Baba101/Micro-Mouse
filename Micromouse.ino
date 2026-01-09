51ef4485bbadc634080c79f6513ea9b1b5a831d0
Mudasir <mohammadmudasirbaba4@gmail.com>
2026-01-09 21:26:00 +0530

    Add files via upload

---
+++ Micromouse.ino
@@ -0,0 +1,33 @@
+#include "floodfill.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  floodfillInit();
+}
+
+void loop() {
+
+  bool f = wallFront(); // wall detection, either ir or ultrasonic or tof idk
+  bool l = wallLeft();
+  bool r = wallRight();
+
+  while (!floodfillAtGoal()) {
+    floodfillUpdateWalls(f, l, r);
+
+    Dir next = floodfillNextMove();
+
+    turnTo(next);          // motor stuff to turn, also will add mpu6050
+    moveOneCell();         // encoders will ensure that it moves exactly 1 cell hopefully
+
+    floodfillSetHeading(next);
+    floodfillAdvance();
+  }
+}
+
+bool wallFront();
+bool wallRight();
+bool wallFront();
+void turnTo(Dir d);
+void moveOneCell();
+
